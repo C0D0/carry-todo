4590e9404bac2db93eff53015a61b5bf9548c14e
martin-call-learning <martin2001.cornumansuy@gmail.com>
2023-05-16 09:59:17 +0200

    Removed todo app component because it was useless

--- todo-json/src/App.vue
+++ todo-json/src/App.vue
@@ -1,31 +1,43 @@
 <template>
-  <todo-app />
+   <div class="todo-app">
+      <h1>To Do List</h1>
+      <todo-form />
+      <todo-list :store="store"/>
+    </div>
 </template>
+<script setup>
+    import { useTodoListStore } from "./store/todoListStore";
+    import TodoForm from "./components/TodoForm.vue";
+    import TodoList from "./components/TodoList.vue";
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import TodoApp from "./components/TodoApp.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    TodoApp,
-  },
-});
+    const app = {
+      components:{
+        TodoList,
+        TodoForm
+      }
+    }
+    const store = useTodoListStore();
+    store._getTodos()
 </script>
 <style>
-* {
-  margin: 0;
-  padding: 0;
-}
-#app {
-  background-color: #597d9a;
-  display: flex;
-  justify-content: center;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    background-color: #597d9a;
+    display: flex;
+    justify-content: center;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  .todo-app {
+    padding: 5vh;
+    min-height: 100vh;
+    width: 30vw;
+    background-color: #f7f9fc;
+  }
 </style>
