f104a14ee5f78a53f566e3020ab31476fee83939
issam-java <issambenaicha@live.fr>
2023-05-18 20:42:04 +0200

    Ajout du style UI/UX

--- todo-json/src/components/TodoList.vue
+++ todo-json/src/components/TodoList.vue
@@ -1,51 +1,64 @@
 <template>
-    <div v-for="todo in todoList" :key="todo.id" class="list">
-      <div class="item">
-        <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
-        <div>
-          <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
-          <span @click="deleteTodo(todo.id)" class="x">&#10060;</span>
-        </div>
+  <div v-for="todo in todoList" :key="todo.id" class="list">
+    <div class="item">
+      <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+      <div>
+        <button @click.stop="toggleCompleted(todo.id)"><svg xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+            <path
+              d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
+          </svg></button>
+        <button @click="deleteTodo(todo.id)" class="x">
+          <svg xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+            <path
+              d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z" />
+          </svg></button>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script lang="ts">
-  import { defineComponent } from "vue";
-  import { useTodoListStore } from "../store/useTodoListStore";
-  import { storeToRefs } from "pinia";
-  export default defineComponent({
-    setup() {
-      const store = useTodoListStore();
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useTodoListStore } from "../store/useTodoListStore";
+import { storeToRefs } from "pinia";
+export default defineComponent({
+  setup() {
+    const store = useTodoListStore();
+
+    const { todoList } = storeToRefs(store);
+    const { toggleCompleted, deleteTodo } = store;
+
+    return { todoList, toggleCompleted, deleteTodo };
+  },
+});
+</script>
   
-      const { todoList } = storeToRefs(store);
-      const { toggleCompleted, deleteTodo } = store;
-  
-      return { todoList, toggleCompleted, deleteTodo };
-    },
-  });
-  </script>
-  
-  <style scoped>
-  span {
-    margin: 0 10px;
-    cursor: pointer;
-  }
-  
-  .completed {
-    text-decoration: line-through;
-  }
-  
-  .list {
-    display: flex;
-    justify-content: center;
-  }
-  
-  .item {
-    display: flex;
-    font-size: 1.5em;
-    justify-content: space-between;
-    width: 80vw;
-    padding: 5px;
-  }
-  </style>
+<style scoped>
+button {
+  margin: 0 10px;
+  background-color: transparent;
+  border: none;
+}
+
+button > svg {
+  height: 20px;
+}
+.completed {
+  text-decoration: line-through;
+  opacity: 20%;
+}
+
+.list {
+  display: flex;
+  justify-content: center;
+}
+
+.item {
+  display: flex;
+  font-size: 1.5em;
+  justify-content: space-between;
+  width: 80vw;
+  padding: 5px;
+}</style>
